13c8ab26d1365300bc41a821c2b7e25a43424d8f
georgepeter2004 <georgepeter200424@gmail.com>
2026-01-12 23:45:45 +0530

    sideview.jsx updated

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import { useNavigate } from "react-router-dom"
+import instagramText from "./assets/instagram text.png";
+
 
 function Sidebar() {
   const navigate = useNavigate();
   return (
     <div className="m-3 position-fixed">
     <div className="d-flex flex-column gap-3">
-        <img className="logo-text m-2" src="./src/assets/instagram text.png" alt="Instagram" />
+        <img className="logo-text m-2" src={instagramText} alt="Instagram" />
         <div><i className="bi bi-house"></i>Home</div>
         <div><i className="bi bi-search"></i>Search</div>
         <div><i className="bi bi-compass"></i>Explore</div>
